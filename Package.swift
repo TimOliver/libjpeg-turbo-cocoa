9b8a24dc9223f18c1da24978d12e734cb78d956c
Tim Oliver <TimOliver@users.noreply.github.com>
2026-03-02 12:17:39 +0000

    Update Package.swift for 3.1.3

--- Package.swift
+++ Package.swift
@@ -23,22 +23,22 @@ let package = Package(
         .binaryTarget(
             name: "turbojpeg",
             url: "\(baseURL)/libjpeg-turbo-v\(version)-xcframework-turbojpeg-static.zip",
-            checksum: "106bf48dca15ffe1af5b821f132c721bafecc432469e9fe58121a87aa3ea4110"
+            checksum: "f1eef005e7236b9b2c93a9abc104933f302ceac49b272467cd8083c91ca428d6"
         ),
         .binaryTarget(
             name: "turbojpeg-dynamic",
             url: "\(baseURL)/libjpeg-turbo-v\(version)-xcframework-turbojpeg-dynamic.zip",
-            checksum: "54b85b4a49b292a3101d128c955a1ad8868c8befb600d2e85dd0dbb7a82ef44c"
+            checksum: "a969e1abaaccbb9985831bd48468ee92eb533b95fd3a2637d05d35b93b0857a3"
         ),
         .binaryTarget(
             name: "libjpeg",
             url: "\(baseURL)/libjpeg-turbo-v\(version)-xcframework-jpeglib-static.zip",
-            checksum: "2ad108eb274513071c589628c0624e4c26266f1932d2f052cca062a8c0ab0ae3"
+            checksum: "44eb9ba8851375c147e4f37e5b311fae186f0506fa581c89dfcaf927e8debaa7"
         ),
         .binaryTarget(
             name: "libjpeg-dynamic",
             url: "\(baseURL)/libjpeg-turbo-v\(version)-xcframework-jpeglib-dynamic.zip",
-            checksum: "467f4530fbebf628c7c1c99216aefa405a4a6ea00a6a91af523d1dc1c872a630"
+            checksum: "79a0c547f3e5b46016b01a066f9311734936f65b2d7de283d0d3deb3a64c45aa"
         ),
     ]
 )
